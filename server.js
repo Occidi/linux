d409455690e16d65eee0f8bb6136c6d8942dcc85
Occidi <gwendal.jeanson@yahoo.fr>
2016-11-30 13:22:59 +0100

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 var express = require('express');
-var app = express();
+
 var exphbs = require('express3-handlebars');
+
+var request = require('request');
+var async = require('async');
+var fs = require('fs');
+var bodyParser   = require('body-parser');
+var passport = require('passport');
+var LdapStrategy = require('passport-ldapauth');
+
+var OPTS = {
+  server: {
+    url: 'ldap://localhost:389',
+    bindDn: 'cn=root',
+    bindCredentials: 'secret',
+    searchBase: 'ou=passport-ldapauth',
+    searchFilter: '(uid={{username}})'
+  },
+    usernameField: 'username',
+    passwordField: 'password'
+};
+
+var app = express();
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
+passport.use(new LdapStrategy(OPTS));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(passport.initialize());
+
+app.post('/login', passport.authenticate('ldapauth', {session: false}), function(req, res) {
+  console.log('test');
+
+  //access admin
+  res.send({status: 'ok'});
+});
+
+app.get('/', function(req, res){
 	res.render('index');
 });
+
 
 app.use(express.static('public'));
 
